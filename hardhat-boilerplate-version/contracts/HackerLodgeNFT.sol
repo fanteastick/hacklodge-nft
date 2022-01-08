bb55feb55c14ddbcf3c94f7b7cbcafcf424a4764
Veronica Zheng <veronica.zheng@coinbase.com>
2022-01-08 11:29:01 -0600

    add hardhat boilerplate version

---
+++ hardhat-boilerplate-version/contracts/HackerLodgeNFT.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+
+// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
+
+contract HackerLodgeNFT is ERC721URIStorage, Ownable {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  string public description = "Hack Lodge 2022!!!";
+
+  constructor() ERC721("HackLodge", "HL") {}
+
+  function mintItem(address to, string memory tokenURI)
+      public
+      onlyOwner
+      returns (uint256)
+  {
+      _tokenIds.increment();
+
+      uint256 id = _tokenIds.current();
+      _mint(to, id);
+      _setTokenURI(id, tokenURI);
+
+      return id;
+  }
+
+  function setDescription(string memory newDescription) public {
+      description = newDescription;
+      console.log(msg.sender, "set desccription to", description);
+  }
+}
