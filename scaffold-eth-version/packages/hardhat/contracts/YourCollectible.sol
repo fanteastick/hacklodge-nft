61430244bfb87555837e047b269d7db3705d5fc3
Veronica Zheng <veronica.zheng@coinbase.com>
2022-01-09 07:45:12 -0800

    add scaffold-eth-version

---
+++ scaffold-eth-version/packages/hardhat/contracts/YourCollectible.sol
@@ -0,0 +1,40 @@
+pragma solidity >=0.6.0 <0.7.0;
+//SPDX-License-Identifier: MIT
+
+//import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+
+// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
+
+contract YourCollectible is ERC721, Ownable {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // string public description = "Hack Lodge 2022!!!";
+
+  constructor() public ERC721("HackLodge", "HL") {
+    _setBaseURI("https://ipfs.io/ipfs/");
+  }
+
+  function mintItem(address to, string memory tokenURI)
+      public
+      onlyOwner
+      returns (uint256)
+  {
+      _tokenIds.increment();
+
+      uint256 id = _tokenIds.current();
+      _mint(to, id);
+      _setTokenURI(id, tokenURI);
+
+      return id;
+  }
+
+  // function setDescription(string memory newDescription) public {
+  //     description = newDescription;
+  // }
+}
