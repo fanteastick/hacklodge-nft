64f1dcfb07aa7a5e20735f851e719fdddb37307a
Veronica Zheng <veronica.zheng@coinbase.com>
2022-01-08 18:31:33 -0600

    add frontend components

--- hardhat-boilerplate-version/hardhat.config.js
+++ hardhat-boilerplate-version/hardhat.config.js
@@ -4,11 +4,11 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.10",
   networks:{
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.GOERLI_PRIVATE_KEY]
-    },
+      url: "https://goerli.infura.io/v3/5667a1f708754d8687b99382f8b3a92a",
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    }
   },
 };
