a415dc9716cc904d19d760be683ce61bd505e703
Veronica Zheng <veronica.zheng@coinbase.com>
2022-01-09 17:21:21 -0800

    cleanup

--- hardhat-boilerplate-version/hardhat.config.js
+++ hardhat-boilerplate-version/hardhat.config.js
@@ -3,12 +3,16 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+// To deploy to goerli testnet, uncomment the code below.
+// You also need to set the enviornment variable GOERLI_PRIVATE_KEY,
+// you can run `export GOERLI_PRIVATE_KEY=<private-key>` in your command line.
 module.exports = {
   solidity: "0.8.0",
-  networks:{
-    goerli: {
-      url: "https://goerli.infura.io/v3/5667a1f708754d8687b99382f8b3a92a",
-      accounts: [process.env.GOERLI_PRIVATE_KEY],
-    },
-  },
+  // networks:{
+  //   goerli: {
+  //     url: "https://goerli.infura.io/v3/5667a1f708754d8687b99382f8b3a92a",
+  //     accounts: [process.env.GOERLI_PRIVATE_KEY],
+  //   },
+  // },
 };
